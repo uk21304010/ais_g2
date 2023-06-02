4f72c7fdf8730bd16e6363c130a0b9f4583515dc
goguma <wnstjd7640@gmail.com>
2023-06-02 16:57:18 +0900

    junseong park 0602

--- src/main/java/command/InputCommand.java
+++ src/main/java/command/InputCommand.java
@@ -5,7 +5,7 @@ import servlet.ResponseContext;
 public class InputCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
 		
-		resc.setTarget("inputDetail");
+		resc.setTarget("getInfoDetail");
 		return resc;
 	}
 }
--- src/main/java/dao/ContentInfoDao.java
+++ src/main/java/dao/ContentInfoDao.java
@@ -7,34 +7,33 @@ import java.sql.SQLException;
 public class ContentInfoDao{
 	private Connection cn = null;
 	private PreparedStatement st = null;
-
+	
 	public void showDetail() {
-		try{
-			cn = ConnectionManager.getInstance().getConnection();
-			String sql = "INSERT INTO `favorite_table` VALUES (?,?);";
-			st = cn.prepareStatement(sql);
-
-
-            int result = st.executeUpdate();
-
-			ConnectionManager.getInstance().commit();
-
-		}catch(SQLException e) {
-			e.printStackTrace();
-			ConnectionManager.getInstance().rollback();
-		}catch(Exception e) {
-			e.printStackTrace();
-			ConnectionManager.getInstance().rollback();
+		try {
+		cn = ConnectionManager.getInstance().getConnection();
+		String sql = " select attraction_name,Attraction_Con,Img_Name "
+				+ " from Keyword_tbl kt, Attraction_tbl at "
+				+ " where at.keyword_num = kt.keyword_Num and kt.keyword_num='?'";
+		
+		int result = st.executeUpdate();
+		
+		ConnectionManager.getInstance().commit();
+	}catch(SQLException e) {
+		e.printStackTrace();
+		ConnectionManager.getInstance().rollback();
+	}catch(Exception e) {
+		e.printStackTrace();
+		ConnectionManager.getInstance().rollback();
 	}finally {
-			if(st != null) {
-				try {
-					st.close();
-				}
-				catch(SQLException e) {
-					e.printStackTrace();
-				}
+		if(st != null) {
+			try {
+				st.close();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
 			}
 		}
 	}
+}
 }
 	
