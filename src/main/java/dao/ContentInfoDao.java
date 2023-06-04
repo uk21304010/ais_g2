592198e2e79960541d39bdcb47dc3a0fe2c3fa29
Sweetpot4t0 <115781324+Sweetpot4t0@users.noreply.github.com>
2023-06-04 21:42:06 +0900

    0604
    0604 with Wookyeong
    commit;

--- src/main/java/dao/ContentInfoDao.java
+++ src/main/java/dao/ContentInfoDao.java
@@ -1,39 +1,51 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import dto.Product;
 
 public class ContentInfoDao{
 	private Connection cn = null;
 	private PreparedStatement st = null;
-	
-	public void showDetail() {
-		try {
-		cn = ConnectionManager.getInstance().getConnection();
-		String sql = " select attraction_name,Attraction_Con,Img_Name "
+	private ResultSet rs = null;
+	private String driver="oracle.jdbc.OracleDriver";
+	private String url="jdbc:oracle:thin:@//localhost:1521/xe";
+
+	public ArrayList<Product> showDetail(int num) {
+		String select = " select attraction_name,Attraction_Con,Img_Name "
 				+ " from Keyword_tbl kt, Attraction_tbl at "
 				+ " where at.keyword_num = kt.keyword_Num and kt.keyword_num='?'";
+
+		ArrayList<Product> showDetail = new ArrayList<Product>();
+			
 		
-		int result = st.executeUpdate();
-		
-		ConnectionManager.getInstance().commit();
-	}catch(SQLException e) {
-		e.printStackTrace();
-		ConnectionManager.getInstance().rollback();
-	}catch(Exception e) {
-		e.printStackTrace();
-		ConnectionManager.getInstance().rollback();
-	}finally {
-		if(st != null) {
+		try {
+			Class.forName(driver);
+			cn=DriverManager.getConnection(url,"hr","hr");
+			st=cn.prepareStatement(select);
+			st.setInt(1, num);
+			rs=st.executeQuery();
+			
+				while(rs.next()) {
+					Product product = new Product();
+					product.setTitlePlace(rs.getString(1));
+					product.setDetailContent(rs.getString(2));
+					product.setThumbnailScreen(rs.getString(3));
+					showDetail.add(product);
+				}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
 			try {
-				st.close();
-			}
-			catch(SQLException e) {
+				rs.close(); st.close(); cn.close();
+				}catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
+		return showDetail; //내가 쓸 처리가 있는 Command로 이게 이동 
 	}
 }
-}
-	
