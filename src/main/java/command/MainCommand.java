0645c682b7bec6d1d2914ca3fba20f0de13661c6
Rose <jung950114@gmail.com>
2023-06-07 13:20:52 +0900

    0607

--- src/main/java/command/MainCommand.java
+++ src/main/java/command/MainCommand.java
@@ -2,10 +2,10 @@ package command;
 
 import servlet.ResponseContext;
 
-public class InputCommand extends AbstractCommand{
+public class MainCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
 		
-		resc.setTarget("inputDetail");
+		resc.setTarget("main");
 		return resc;
 	}
 }
