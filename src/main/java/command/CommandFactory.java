ce54a72132cd0aa6e4f3dd6b9b0e17729052c8b4
kim <uk21304010@ga.ttc.ac.jp>
2023-06-07 15:23:37 +0900

    ss

--- src/main/java/command/CommandFactory.java
+++ src/main/java/command/CommandFactory.java
@@ -7,27 +7,14 @@ import java.util.Properties;
 
 import servlet.RequestContext;
 
-public abstract class CommandFactory{ //처리는 커맨드에서 커맨드가 다오를 부른다.
+public abstract class CommandFactory{ /
 	public static AbstractCommand getCommand(RequestContext rc){
 		AbstractCommand command = null;
 		Properties prop = new Properties();
 		
 		try{
-				
-<<<<<<< HEAD
-			prop.load(new FileInputStream("C:\\Users\\AIS-N\\eclipse-workspace\\ais_g2\\src\\main\\java\\command\\commands.properties"));
-=======
-<<<<<<< HEAD
 
 			prop.load(new FileInputStream("C:\\Users\\user\\eclipse-workspace\\ais_g2\\src\\main\\java\\command\\commands.properties"));
-=======
-<<<<<<< HEAD
-			prop.load(new FileInputStream("C:\\Users\\AIS-N\\eclipse-workspace\\ais_g2\\src\\main\\java\\command\\commands.properties"));
-=======
-			prop.load(new FileInputStream("C:\\Eclipse-workspace\\ais_g2\\src\\main\\java\\command\\commands.properties"));
->>>>>>> a9746dcdad974bc570ffe8d0277b74bab3a8345a
->>>>>>> 95a8a3d10af4e580d7b3757042fa899edf9573ff
->>>>>>> 4bced9fd848c86090c9efd21461d30b5c54a2681
 
 			String name = prop.getProperty(rc.getCommandPath());
 
--- src/main/java/dao/AccountDao.java
+++ src/main/java/dao/AccountDao.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import dto.Product;
+import src.main.java.dto.Product;
 
 public class AccountDao{
 
